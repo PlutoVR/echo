714a48a774952ef35548e38710e9e7b5c6c10e01
Jocelyn <jocegonz@gmail.com>
2021-01-25 12:29:23 -0800

    added gitignore; more logic;

--- main.lua
+++ main.lua
@@ -1,12 +1,30 @@
-local speech = require('deepspeech')
+local speech = require 'lua-deepspeech'
+local root = lovr.filesystem.getRealDirectory('data')
+
 function lovr.load()
-  --
+  microphone = lovr.audio.newMicrophone(nil, 1024, 16000, 16, 1)
+  microphone:startRecording()
+
+  speech.init({
+    model = root .. '/data/deepspeech-0.9.3-models.pbmm',
+    scorer = root .. '/data/deepspeech-0.9.3-models.scorer'
+  })
+
+  sampleRate = speech.getSampleRate()
+  assert(sampleRate == 16000, string.format('Unsupported sample rate %d', sampleRate))
+  stream = speech.newStream()
 end
 
 function lovr.update(dt)
-  --
+  if microphone:getSampleCount() > 1024*5 then
+    local soundData = microphone:getData()
+    stream:feed(soundData:getBlob():getPointer(), soundData:getSampleCount()) 
+    print(stream:decode())
+    stream:clear()  
+  end
 end
 
 function lovr.draw()
   lovr.graphics.cube('fill')
+  lovr.graphics.print('hello world', 0, 1.7, -3, .5)
 end
