cd36b8af4fe5d6f872d89f74f6955ac2154d116c
sophia <sophiabaldonado@gmail.com>
2021-01-29 08:18:04 -0800

    only render raycast lines when hovering;

--- control-panel.lua
+++ control-panel.lua
@@ -53,6 +53,7 @@ function control:init()
   self.buttonHeight = .2*self.buttonScale
   self.buttonWidth = .2*self.buttonScale
   self.offsetX, self.offsetY, self.offsetZ = 0, 0, 0
+  self.hovering = {}
 
   self.tips = {}
 end
@@ -72,6 +73,7 @@ function control:raycast(rayPos, rayDir, planePos, planeDir)
 end
 
 function control:updatePointer()
+  self.hovering = {}
   for i, hand in ipairs(lovr.headset.getHands()) do
     self.tips[hand] = self.tips[hand] or lovr.math.newVec3()
 
@@ -93,6 +95,7 @@ function control:updatePointer()
       -- If the ray intersects the plane, do a bounds test to make sure the x/y position of the hit
       -- is inside the button, then mark the button as hover/active based on the trigger state.
       if inside then
+        table.insert(self.hovering, hand)
         if lovr.headset.wasReleased(hand, 'trigger') then
           button.onClick()
         end
@@ -140,7 +143,7 @@ function control:drawPointer()
     lovr.graphics.setColor(1, 1, 1)
     lovr.graphics.sphere(position, .01)
 
-    if self.settings.active then
+    if self.settings.active and #self.hovering > 0 and self.hovering[1] == hand then
       lovr.graphics.line(position, tip)
       lovr.graphics.setColor(1, 1, 1)
     end
